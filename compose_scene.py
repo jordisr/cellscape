a130183b9e49e829e8c6f9992f0de09980bf98d3
jordisr <jordisr@users.noreply.github.com>
2019-06-19 11:55:47 -0700

    testing new options for scene generation

--- compose_scene.py
+++ compose_scene.py
@@ -17,21 +17,24 @@ parser.add_argument('--offsets', nargs='+', default=[], help='Vertical offsets f
 parser.add_argument('--padding', type=int, default=0, help='Horizontal padding to add between each molecule (in angstroms)')
 parser.add_argument('--axes', action='store_true', default=False, help='Draw x and y axes')
 parser.add_argument('--format', default='png', help='Format to save graphics', choices=['svg','pdf','png'])
-parser.add_argument('--membrane', default=None, choices=[None, 'fancy', 'box'], help='Draw membrane on X axis')
+parser.add_argument('--membrane', default=None, choices=[None, 'box', 'fancy', 'extra_fancy'], help='Draw membrane on X axis')
 parser.add_argument('--dpi', type=int, default=300, help='DPI to use if exporting to raster formats (i.e. PNG)')
 
 # for simulating according to stoichiometry
 parser.add_argument('--csv', help='Table of protein information')
+parser.add_argument('--sample_from', help='Column to use for sampling', default='stoichiometry')
 parser.add_argument('--num_mol', type=int, help='Total number of molecules in the scene')
 parser.add_argument('--order_by', default='input', choices=['input', 'random', 'height'], help='How to order proteins in scene')
 parser.add_argument('--background', action='store_true', default=False, help='Add background plane using same frequencies')
+parser.add_argument('--testing', action='store_true', default=False, help='Experimental option')
+
 
 args = parser.parse_args()
 
 def draw_membrane(width, height=40):
     axs = plt.gca()
     membrane_box = mpatches.FancyBboxPatch(
-        [-100, 0], 1.5*width, -1*height,
+        [-100, 10], 1.5*width, -1*height+10,
         boxstyle=mpatches.BoxStyle("Round", pad=0.02),
         facecolor='#DDD5C7', ec='#DDD5C7', alpha=1, zorder=2)
     axs.add_patch(membrane_box)
@@ -47,20 +50,24 @@ def draw_membrane_fancy(width, height=40, jitter=False):
     lipid_tail_fc='#A3DCEF'
     membrane_box_fc='#C4E7EF'
 
-    # membrane box background
-    membrane_box = mpatches.FancyBboxPatch(
-        [-100, -1*head_radius+10], 1.5*width, -1*height+head_radius,
-        boxstyle=mpatches.BoxStyle("Round", pad=0.02),
-        facecolor=membrane_box_fc, alpha=1, zorder=1)
-    axs.add_patch(membrane_box)
+    if not jitter:
+        # membrane box background
+        membrane_box = mpatches.FancyBboxPatch(
+            [-100, -1*head_radius+10], 1.5*width, -1*height+head_radius,
+            boxstyle=mpatches.BoxStyle("Round", pad=0.02),
+            facecolor=membrane_box_fc, edgecolor='none', alpha=1, zorder=1)
+        axs.add_patch(membrane_box)
 
     # draw each lipid pair
     for i in range(num_lipids):
         top_jitter_y = 10
         bot_jitter_y = 10
         if jitter:
-            top_jitter_y += (np.random.random()-0.5)*2*head_radius
-            bot_jitter_y += (np.random.random()-0.5)*2*head_radius
+            sin_y = 2*head_radius*np.sin(i/num_lipids*20*np.pi)
+            top_jitter_y += sin_y
+            bot_jitter_y += sin_y
+            #top_jitter_y += (np.random.random()-0.5)*2*head_radius
+            #bot_jitter_y += (np.random.random()-0.5)*2*head_radius
         axs.add_line(mlines.Line2D([i*head_radius*2-100, i*head_radius*2-100], [-4+top_jitter_y, -18+top_jitter_y], zorder=1.5, c=lipid_tail_fc, linewidth=head_radius*.7, alpha=1, solid_capstyle='round'))
         axs.add_line(mlines.Line2D([i*head_radius*2-100, i*head_radius*2-100], [-38+bot_jitter_y, -24+bot_jitter_y], zorder=1.5, c=lipid_tail_fc, linewidth=head_radius*.7, alpha=1, solid_capstyle='round'))
         axs.add_patch(mpatches.Circle((i*head_radius*2-100, -1*head_radius+top_jitter_y), head_radius, facecolor=lipid_head_fc, ec='k', linewidth=0.3, alpha=1, zorder=2))
@@ -75,15 +82,19 @@ if args.files:
         with open(path,'rb') as f:
             data = pickle.load(f)
             object_list.append(data)
+            coords = np.concatenate([p.get_xy() for p in data['polygons']])
+            data['height'] = np.max(coords[:,1]) # temporary bugfix
     num_files = len(args.files)
 elif args.csv:
     protein_data = dict()
     with open(args.csv) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            (name, stoich, path) = (row['name'], int(row['stoichiometry']), row['file'])
-            with open(path,'rb') as f:
+            (name, stoich, path) = (row['name'], float(row[args.sample_from]), row['file'])
+            with open(path+'.pickle','rb') as f:
                 data = pickle.load(f)
+                data['name'] = name
+                data['stoichiometry'] = stoich
                 protein_data[name] = (stoich, data)
     num_files = len(protein_data)
 else:
@@ -113,7 +124,7 @@ if args.csv:
 
     # assemble objects for background
     if args.background:
-        scaling_factor = 0.6
+        scaling_factor = 0.7
         sampled_protein = np.random.choice(protein_names, int(num_mol*1/scaling_factor), p=stoich_weights)
         background_object_list = [protein_data[p][1] for p in sampled_protein]
 
@@ -143,6 +154,15 @@ else:
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
+if args.testing:
+    # colors
+    cmap = cm.get_cmap('hsv')
+    #cmap1 = cm.get_cmap('Set1')
+    #cmap2 = cm.get_cmap('Pastel1')
+    color_scheme = dict()
+    for i,c in enumerate(protein_names):
+        color_scheme[c] = cmap(i/len(protein_names))
+
 # draw molecules
 w=0
 for i, o in enumerate(object_list):
@@ -153,7 +173,11 @@ for i, o in enumerate(object_list):
         lw=0.5
     for p in o['polygons']:
         xy = p.get_xy()
-        axs.fill(xy[:,0]+w, xy[:,1], fc=p.get_facecolor(), ec='k', linewidth=lw, zorder=3)
+        if args.testing:
+            axs.fill(xy[:,0]+w, xy[:,1], fc=color_scheme[o['name']], ec='k', linewidth=lw, zorder=3)
+        else:
+            axs.fill(xy[:,0]+w, xy[:,1], fc=p.get_facecolor(), ec='k', linewidth=lw, zorder=3)
+
     w += o['width']+args.padding
 
 if args.background:
@@ -161,7 +185,10 @@ if args.background:
     for i, o in enumerate(background_object_list):
         for p in o['polygons']:
             xy = p.get_xy()
-            axs.fill((xy[:,0]+background_w)*scaling_factor, (xy[:,1])*scaling_factor, fc=p.get_facecolor(), linewidth=0, zorder=1)
+            if args.testing:
+                axs.fill((xy[:,0]+background_w)*scaling_factor, (xy[:,1])*scaling_factor, fc=color_scheme[o['name']], ec='k', alpha=0.8, linewidth=lw*scaling_factor, zorder=1)
+            else:
+                axs.fill((xy[:,0]+background_w)*scaling_factor, (xy[:,1])*scaling_factor, fc=p.get_facecolor(), ec='k', alpha=0.8, linewidth=lw*scaling_factor, zorder=1)
         background_w += (o['width']+args.padding)
 
 if args.membrane is not None:
@@ -170,5 +197,7 @@ if args.membrane is not None:
         draw_membrane(width=w)
     elif args.membrane == 'fancy':
         draw_membrane_fancy(width=w)
+    elif args.membrane == 'extra_fancy':
+        draw_membrane_fancy(width=w, jitter=True)
 
 plt.savefig(args.save+'.'+args.format, transparent=True, pad_inches=0, bbox_inches='tight', dpi=args.dpi)
